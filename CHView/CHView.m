bd10b557e2d3c4f8e0626339a0fbcd406d86d019
chenghao0069 <chenghao@fitcare.me>
2017-03-24 11:36:17 +0800

    第七次提交

--- CHView/CHView.m
+++ CHView/CHView.m
@@ -36,7 +36,8 @@
     bezelView.layer.cornerRadius = 5;
     [bezelView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.center.equalTo(0);
-        make.size.equalTo(CGSizeMake(108, 90));
+        make.height.equalTo(90);
+        make.width.equalTo(108);
     }];
 }
 
